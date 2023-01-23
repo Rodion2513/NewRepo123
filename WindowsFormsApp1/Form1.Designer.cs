c57226f90ab70fe6787f9221e73aecc3d83edbec
V1adimir1232 <vov422nikif@mail.ru>
2023-01-23 13:15:29 +0900

    армпармармра

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -57,6 +57,7 @@ namespace WindowsFormsApp1
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // eventLog1
             // 
